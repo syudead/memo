455b1116f3f9acbd40567e6908d557e5ef8d4c96
syudead <syudead@gmail.com>
2020-03-03 00:48:53 +0900

    init bootstrap

--- src/components/Memo.tsx
+++ src/components/Memo.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const Memo = () => {
 
   return (
     <div>
+      <Button>this is button</Button>
       <textarea></textarea>
     </div>
   )
